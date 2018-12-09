7ca7e1ef063ec31d6f56b59751475c4ff6e62043
Tanooj Shah <tanooj.s.shah@gmail.com>
2018-12-09 13:23:25 -0800

    added constant-q transform to output

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import numpy as np
 import pandas as pd
 import librosa.core as lc
 from librosa import amplitude_to_db
-from matplotlib.figure import Figure
-#from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import matplotlib.pyplot as plt
 import librosa.display
+import librosa.effects
+
+import matplotlib.pyplot as plt
+
 from scipy import fftpack
 
 class UploadForm(FlaskForm):
@@ -31,31 +32,49 @@ def upload():
 		# now the file has been uploaded by the user, we need to read it in from the uploads folder using librosa
 		curr_time = str(time.time())[:10] # get epoch time without decimals to save to plt image later
 		samples, rate = lc.load(path='uploads/'+str(filename),duration=15,mono=True)
+		samples, _ = librosa.effects.trim(samples,top_db=20)
+		fourier_transform = get_fft(samples,rate)
+		constant_q = librosa.amplitude_to_db(np.abs(lc.cqt(samples)),ref=np.max)
+	
+		fig = plt.figure(figsize=(15,9))	
 
-		nyquist = int(rate/2)
-		yfft = fftpack.fft(x=samples,n=rate)
-		max_amp = np.max(np.abs(yfft))
-		xfft = np.linspace(0,nyquist,nyquist)
-		yfft = np.abs(yfft[:nyquist])/(max_amp) # not sure why we don't need to multiply by 2/len(samples) to scale properly here
-		
-		fig = plt.figure(figsize=(15,6))
-		axis = fig.add_subplot(1,1,1)
-		axis.set_xlabel('Frequency (Hz)')
-		axis.set_ylabel('Relative Amplitude')
-		axis.set_xlim(20,20000) # human range
-		axis.set_xticklabels(np.arange(100,20000,100))
-		axis.semilogx(xfft,yfft)
+		axis1 = fig.add_subplot(2,1,1)
+		axis1.set_title('Constant Q Transform')
+		axis1.set_xlabel('Time (s)')
+		axis1.set_ylabel('Notes - 12 per octave')
+		axis1.pcolormesh(constant_q)
+		axis1.set_yticklabels(np.arange(0,constant_q.shape[1],12))
+
+		axis2 = fig.add_subplot(2,1,2)
+		axis2.set_ylabel('Relative Amplitude')
+		axis2.set_xlabel('Frequency (Hz)')
+		axis2.set_xlim(20,20000) # human range
+		axis2.set_xticklabels(np.arange(100,20000,100))
+		axis2.semilogx(fourier_transform['x'],fourier_transform['y'])
+
+		fig.tight_layout()
 		plot_path = 'static/img/plot_' + curr_time + '.png'
 		fig.savefig(plot_path)
+		#each plot should be its own image, separate objects for spectrograms and frequency-space plots
 
 		session['path_to_plot'] = plot_path		
-		return redirect(url_for('fast_fourier'))
+		return redirect(url_for('results'))
+
 	return render_template('upload.html', form=form)
 
 
 @app.route('/fft',methods=['GET','POST'])
-def fast_fourier():
-	return render_template('fft_plot.html')
+def results():
+	return render_template('results.html')
+
+
+def get_fft(samples,rate):
+	nyquist = int(rate/2)
+	yfft = fftpack.fft(x=samples,n=rate)
+	max_amp = np.max(np.abs(yfft))
+	xfft = np.linspace(0,nyquist,nyquist)
+	yfft = np.abs(yfft[:nyquist])/(max_amp) # not sure why we don't need to multiply by 2/len(samples) to scale properly here
+	return {'x': xfft, 'y': yfft}
 
 
 if __name__=='__main__':
