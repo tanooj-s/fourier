c3882623e58f46a1785fa869791e7b961bc43d01
Tanooj Shah <tanooj.s.shah@gmail.com>
2019-01-02 21:04:38 +0530

    Added option to download harmonic and percussive components

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
 from flask import url_for, redirect, render_template, Flask, session
 from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField
 from flask_wtf.file import FileField
 from werkzeug import secure_filename
 
 import time
 import numpy as np
-import pandas as pd
 import librosa.core as lc
 from librosa import amplitude_to_db
 import librosa.display
 import librosa.effects
 import librosa.decompose
+from librosa.feature import chroma_cqt
+from librosa.output import write_wav
 
 import matplotlib.pyplot as plt
 
@@ -19,6 +21,8 @@ from scipy import fftpack
 
 class UploadForm(FlaskForm):
 	user_file = FileField()
+	lower_bound_time = FloatField()
+	upper_bound_time = FloatField()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AHs0JleAhvzcolWG'
@@ -27,50 +31,63 @@ app.config['UPLOADED_FILES_DEST'] = 'uploads/'
 @app.route('/',methods=['GET','POST'])
 def upload():
 	form = UploadForm()
+
 	if form.validate_on_submit():
-		filename = secure_filename(form.user_file.data.filename)
-		form.user_file.data.save('uploads/' + filename)
-		# now the file has been uploaded by the user, we need to read it in from the uploads folder using librosa
-		curr_time = str(time.time())[:10] # get epoch time without decimals to save to plt image later
-		samples, rate = lc.load(path='uploads/'+str(filename),duration=15,mono=True)
-		samples, _ = librosa.effects.trim(samples,top_db=20)
-		fourier_transform = get_fft(samples,rate)
-		constant_q = librosa.amplitude_to_db(np.abs(lc.cqt(samples)),ref=np.max)
-		# get harmonic and percussive components using built in librosa 
-		h,p = librosa.amplitude_to_db(librosa.decompose.hpss(np.abs(lc.stft(samples))),ref=np.max) 
+		# save user's uploaded file
+		file_name = secure_filename(form.user_file.data.filename)
+		form.user_file.data.save('uploads/' + file_name)
+		# get user inputted upper and lower bounds
+		low = form.lower_bound_time.data
+		up = form.upper_bound_time.data
 		
-		fig = plt.figure(figsize=(15,12))	
-		axis1 = fig.add_subplot(4,1,1)
-		axis1.set_title('Constant Q Transform')
-		axis1.set_xlabel('Time (s)')
-		axis1.set_ylabel('Notes - 12 per octave')
-		axis1.pcolormesh(constant_q)
-		axis1.set_yticklabels(np.arange(0,constant_q.shape[1],12))
+		curr_time = str(time.time())[:10] # get epoch time without decimals to save to files later
+		samples, rate = lc.load(path='uploads/'+str(file_name),mono=True,duration=up)
 
+		samples, _ = librosa.effects.trim(samples[int(rate*low):int(rate*up)],top_db=20)
 
-		# in addition to a plot what this needs is a widget that allows you to pay the decomposed audio files 
-		# (and possibly a download link for them)
-		axis2 = fig.add_subplot(4,1,2)
-		axis2.set_title('Harmonic Components')
-		axis2.pcolormesh(h)
-		# need to scale the y axis to log scale here
-		axis3 = fig.add_subplot(4,1,3)
-		axis3.set_title('Percussive Components')
-		axis3.pcolormesh(p)
+		harmonic = librosa.effects.harmonic(samples, margin = 2.0)
+		percussive = librosa.effects.percussive(samples, margin = 2.0) 
+		fourier_transform = get_fft(harmonic,rate) # only get the fourier transform of the harmonic
+		
+		cmap = plt.get_cmap('Blues')
+		plt.figure(figsize=(12,15))
 
-		axis4 = fig.add_subplot(4,1,4)
-		axis4.set_ylabel('Relative Amplitude')
-		axis4.set_xlabel('Frequency (Hz)')
-		axis4.set_xlim(20,20000) # human range
-		axis4.set_xticklabels(np.arange(100,20000,100))
-		axis4.semilogx(fourier_transform['x'],fourier_transform['y'])
+		# plot waveform amplitude
+		plt.subplot(5,1,1, title='Waveform', xlabel='Time (s)', ylabel='Amplitude')
+		plt.plot(np.arange(0,len(samples))/rate, samples)
 
-		fig.tight_layout()
-		plot_path = 'static/plot_' + curr_time + '.png'
-		fig.savefig(plot_path)
-		#each plot should be its own image, separate objects for spectrograms and frequency-space plots
+		# plot fourier transform
+		plt.subplot(5,1,2, title='Fourier Transform', xlim=(20,20000), xlabel='Hz', ylabel='Relative Amplitude')
+		plt.semilogx(fourier_transform['x'], fourier_transform['y'])
+		# try doing this only for harmonic components instead
 
-		session['path_to_plot'] = plot_path		
+		# plot spectrogram of harmonic components
+		plt.subplot(5,1,3, title='Spectrogram')
+		librosa.display.specshow(amplitude_to_db(np.abs(lc.stft(harmonic)),ref=np.max), y_axis='log', x_axis='time', cmap=cmap)
+
+		# plot percussive components 
+		plt.subplot(5,1,4, title='Percussive Components')
+		librosa.display.specshow(amplitude_to_db(np.abs(lc.stft(percussive)),ref=np.max), y_axis='log', x_axis='time', cmap=cmap)
+
+		# plot chromagram of harmonic components (try different librosa variants)
+		chromagram = chroma_cqt(harmonic, rate, hop_length=512)
+		plt.subplot(5,1,5, title='Chromagram')
+		librosa.display.specshow(chromagram, y_axis='chroma', x_axis='time', cmap=cmap)
+
+		plt.tight_layout()
+		plot_file = 'static/plot_' + curr_time + '.png'
+		plt.savefig(plot_file)
+
+		# save harmonic and percussive breakdown for user to download
+		h_file = 'static/h_' + curr_time + '.wav'
+		p_file = 'static/p_' + curr_time + '.wav'
+		write_wav(path=h_file, y=harmonic, sr=rate)
+		write_wav(path=p_file, y=percussive, sr=rate)
+
+		session['plots'] = plot_file
+		session['harmonic'] = h_file
+		session['percussive'] = p_file
+
 		return redirect(url_for('results'))
 
 	return render_template('upload.html', form=form)
