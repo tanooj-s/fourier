d521661da55632565f8ac75aac7f5ea2ffc540ec
Tanooj Shah <tanooj.s.shah@gmail.com>
2019-01-05 12:30:09 +0530

    added utility file for helper functions

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from werkzeug import secure_filename
 
 import time
 import numpy as np
+import matplotlib.pyplot as plt
+
 import librosa.core as lc
 from librosa import amplitude_to_db
 import librosa.display
@@ -15,9 +17,8 @@ import librosa.decompose
 from librosa.feature import chroma_stft
 from librosa.output import write_wav
 
-import matplotlib.pyplot as plt
-
 from scipy import fftpack
+from util import get_fft, purge, get_peak_frequencies
 
 class UploadForm(FlaskForm):
 	user_file = FileField()
@@ -102,70 +103,6 @@ def upload():
 def results():
 	return render_template('results.html')
 
-
-def get_fft(samples,rate):
-	nyquist = int(rate/2)
-	yfft = fftpack.fft(x=samples,n=rate)
-	max_amp = np.max(np.abs(yfft))
-	xfft = np.linspace(0,nyquist,nyquist)
-	yfft = np.abs(yfft[:nyquist])/(max_amp) 
-	return {'x': xfft, 'y': yfft}
-
-def get_peak_frequencies(lst):
-	# for this use case lst is assumed to be fft['y'] i.e. the relative amplitudes of the fourier tranform
-	indices = sorted(np.argpartition(lst,-15)[-15:]) # get indices of 15 max values in the list (these essentially correspond to fft['x'])
-	peak_freqs = []
-	for i in indices:
-		if lst[i] > 0.3:
-			peak_freqs.append(i)
-	peak_freqs = purge(peak_freqs) # only retain mean of clusters
-	peak_freqs = [str(p) for p in peak_freqs]
-	return peak_freqs
-
-def purge(lst):
-	lst = sorted(lst)
-	stack = []
-	out = []
-	for num in lst:
-		if len(stack) == 0:
-			stack.append(num)
-		elif (num - stack[-1]) < 5: # peak frequency "cluster" threshold
-			stack.append(num)
-		else:
-			out.append(np.mean(stack))
-			stack = []
-			stack.append(num)
-	if len(stack) > 0:
-		out.append(np.mean(stack))
-	return out	
-
-def get_peak_frequencies(lst):
-	# for this use case lst is assumed to be fft['y'] i.e. the relative amplitudes of the fourier tranform
-	indices = sorted(np.argpartition(lst,-10)[-10:]) # get indices of 15 max values in the list (these essentially correspond to fft['x'])
-	peak_freqs = []
-	for i in indices:
-		if lst[i] > 0.3:
-			peak_freqs.append(i)
-	peak_freqs = purge(peak_freqs) # only retain mean of clusters
-	peak_freqs = [str(p) for p in peak_freqs]
-	return peak_freqs
-
-def purge(lst):
-	lst = sorted(lst)
-	stack = []
-	out = []
-	for num in lst:
-		if len(stack) == 0:
-			stack.append(num)
-		elif (num - stack[-1]) < 5: # peak frequency "cluster" threshold
-			stack.append(num)
-		else:
-			out.append(np.mean(stack))
-			stack = []
-			stack.append(num)
-	if len(stack) > 0:
-		out.append(np.mean(stack))
-	return out	
 
 if __name__=='__main__':
 	app.run(debug=True)
